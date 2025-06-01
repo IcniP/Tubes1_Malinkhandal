f5024dace1837bb7118858ba0aa48a958016f1f6
Pinci <destructioner717@gmail.com>
2025-06-01 16:53:31 +0700

    changed name

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from game.bot_handler import BotHandler
 from game.logic.random import RandomLogic
 from game.util import *
 from game.logic.base import BaseLogic
-from game.logic.greedybot import GreedyBot
+from game.logic.malinkhandal import GreedyBot
 
 init()
 BASE_URL = "http://localhost:3000/api"
